e9b6e2279b4c9dd2259948b85fe3fa6984bfed26
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-07-17 16:44:09 -1000

    Update 4. Plot_Maxent.R

--- script/4. Plot_Maxent.R
+++ script/4. Plot_Maxent.R
@@ -30,10 +30,10 @@ load("output/maxent_result_Unomia stolonifera.rda")
 
 plot(maxent_result$model)
 
-clipped_rasters <- spp_clip_raster(occ_df, env_rs, "SAMOA", 5000)#; plot(clipped_rasters[[1]])
+clipped_raster <- spp_clip_raster(occ_df, env_rs, "Tutuila", 500)#; plot(clipped_raster[[1]])
 # env_rs <- terra::resample(rast(env_rs), rast(bathy))
 
-r <- predict(maxent_result$model, clipped_rasters)
+r <- predict(maxent_result$model, clipped_raster)
 plot(r, col = matlab.like(100))
 r = rasterToPoints(raster(r)) %>% as.data.frame()
 
@@ -49,11 +49,11 @@ mean_lon <- mean(coords[, 1], na.rm = TRUE)
 
 map = ggmap::get_map(location = c(mean_lon, mean_lat),
                      maptype = "satellite",
-                     zoom = 7,
+                     # zoom = 7,
                      force = T)
 ggmap(map) +
   geom_spatial_point(data = r, aes(x, y, fill = maxent, color = maxent), 
-                     # size = 10,
+                     size = 10,
                      shape = 22, alpha = 0.8, crs = 4326) + 
   scale_fill_gradientn(colors = matlab.like(100), "Predicted \nOccupancy \n(0-1)") + 
   scale_color_gradientn(colors = matlab.like(100), "Predicted \nOccupancy \n(0-1)") + 
